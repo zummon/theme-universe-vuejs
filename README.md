69ce145047a2bb9891e26e6ff47b9e19c07c7a5b
CodeSandbox <support@codesandbox.io>
2023-04-01 05:56:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+somehow error, maybe impossible to fix for this beta container.
+
+[Made by zummon](https://zummon.page/)
+
+## Dev tools
+
+- [Bootstrap](https://getbootstrap.com/)
+- [Icons](https://icons.getbootstrap.com/)
+- [unDraw](https://undraw.co/)
---
+++ src/themes.js
@@ -0,0 +1,30 @@
+// store all themes as an array of objects
+export default [
+  {
+    type: "blue",
+    name: "Blue",
+    plate: "bg-light text-primary",
+    svgcolor: "",
+    card: "text-white bg-primary",
+    link: "text-white",
+    button: "btn-outline-primary",
+  },
+  {
+    type: "dark",
+    name: "Dark",
+    plate: "bg-dark text-white",
+    svgcolor: "text-info",
+    card: "text-white bg-info",
+    link: "text-white",
+    button: "btn-outline-light",
+  },
+  {
+    type: "red",
+    name: "Red",
+    plate: "text-danger",
+    svgcolor: "",
+    card: "text-white bg-danger",
+    link: "text-white",
+    button: "btn-outline-danger",
+  },
+];
